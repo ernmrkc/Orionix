a34e9e13fe7dcf5f53c86cdc7e011b8026bb1f57
Muhammed Eren Morkoç <35144373+ernmrkc@users.noreply.github.com>
2024-04-24 18:50:47 +0300

    Update FBlockPool.h

--- Source/Orionix/FBlockPool.h
+++ Source/Orionix/FBlockPool.h
@@ -11,69 +11,69 @@ class ORIONIX_API FBlockPool
 	 */
 public:
 	/**
-	 * @brief				Constructor of FBlockPool class
+	 * @brief			Constructor of FBlockPool class
 	 */
 	FBlockPool();
 
 	/**
-	 * @brief				Desturctor of FBlockPool class
+	 * @brief			Desturctor of FBlockPool class
 	 */
 	~FBlockPool();
 
 	/**
-	 * @brief				Initializes the block pool with specified parameters and pre-populates it with blocks.
-	 *						It ensures the pool is ready with pre-created blocks for use in the game, optimizing runtime performance by avoiding dynamic allocations.
-	 *						All mesh paths are defined in meshPahts to create and add blocks to the pool.
+	 * @brief			Initializes the block pool with specified parameters and pre-populates it with blocks.
+	 *				It ensures the pool is ready with pre-created blocks for use in the game, optimizing runtime performance by avoiding dynamic allocations.
+	 *				All mesh paths are defined in meshPahts to create and add blocks to the pool.
 	 * @param World			The game world context where the blocks will be spawned.
-	 * @param BlockClass	The subclass of ABlock to be used for creating new block instances.
+	 * @param BlockClass		The subclass of ABlock to be used for creating new block instances.
 	 */
 	void initializePool(UWorld *World, TSubclassOf<ABlock> BlockClass);
 
 	/**
-	 * @brief				Removes and returns the last block from the available blocks pool
-	 * @return				Last block of the block pool
+	 * @brief			Removes and returns the last block from the available blocks pool
+	 * @return			Last block of the block pool
 	 */
 	ABlock *popBlock();
 
 	/**
-	 * @brief				Removes and returns a random block from the available blocks pool
-	 * @return				Random block in the block pool
+	 * @brief			Removes and returns a random block from the available blocks pool
+	 * @return			Random block in the block pool
 	 */
 	ABlock *getBlockRandomly();
 
 	/**
-	 * @brief				Returns a block based on the index of the given parameter
+	 * @brief			Returns a block based on the index of the given parameter
 	 * @param index			Index value to be taken from the pool
-	 * @return				Corresponding block in the block pool
+	 * @return			Corresponding block in the block pool
 	 */
 	ABlock *getBlockByIndex(int32 index);
 
 	/**
-	 * @brief				Returns available block numbers in the block pool
-	 * @return				Number of blocks available
+	 * @brief			Returns available block numbers in the block pool
+	 * @return			Number of blocks available
 	 */
 	int32 getPoolSize();
 
 	/**	
-	 * @brief				Returns a block to the pool making it available for reuse
+	 * @brief			Returns a block to the pool making it available for reuse
 	 * @param block			The block to be returned to the pool
 	 */
 	void returnBlock(ABlock *block);
 
 	/**
-	 * @brief				Prints pool status
+	 * @brief			Prints pool status
 	 */
 	void getPoolStatus() const;
 
 private:
 	/**
-	 * @brief				Creates a block to add to the pool and adjust its static mesh.
+	 * @brief			Creates a block to add to the pool and adjust its static mesh.
 	 * @param meshPath		The path of the static mesh to be set
 	 */
 	void createBlock(const FString &meshPath);
 
 	/**
-	 * @brief				Populates the meshPath list with the paths of the static meshes to use
+	 * @brief			Populates the meshPath list with the paths of the static meshes to use
 	 */
 	void initializeMeshPath();
 
@@ -81,8 +81,8 @@ private:
 	 * **VARIABLE DECLARATIONS**
 	 */
 private:
-	UWorld *worldContext;						// The game world context where blocks are spawned
+	UWorld *worldContext;					// The game world context where blocks are spawned
 	TSubclassOf<ABlock> blockBlueprint;			// Blueprint class for creating new blocks
-	TArray<FString> meshPaths;					// Paths to meshes used for block appearances
+	TArray<FString> meshPaths;				// Paths to meshes used for block appearances
 	TArray<ABlock *> availableBlocks;			// Pool of blocks available for use
 };
